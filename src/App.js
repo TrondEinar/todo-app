487606e1dc9bbf5e012f2ff962cfbf0c950bb162
TrondEinar <trond-einar98@hotmail.no>
2021-05-24 15:26:48 +0200

    Form-component and styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
+import "./App.css";
+import Form from "./components/Form";
 
 function App() {
   return (
-    <div> Hello World </div>
+    <div className="App">
+      <header>
+        <h1> Trond Einar´s Todo List </h1>
+      </header>
+      <Form />
+    </div>
   );
 }
 
